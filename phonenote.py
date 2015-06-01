b7676590c7a6ec0b75e25f8ea09e8d87f436653a
Alex Miles Younger <alex@alexmilesyounger.com>
2015-06-01 12:33:03 -0400

    Add handle-recording url

--- phonenote.py
+++ phonenote.py
@@ -13,9 +13,20 @@ def phone_note():
     from_number = request.values.get('From', None)
     resp = twilio.twiml.Response()
     resp.say("Hello Robert. Record your message after the tone.")
-    resp.record(maxLength="300", action="/handle-recording")
+    # maxLength 3600 = one hour
+    resp.record(maxLength="3600", action="/handle-recording", finishOnKey="")
+    return str(resp)
+
+@app.route("/handle-recording", methods=['GET', 'POST'])
+def handle_recording():
+    """Play back the caller's recording."""
+ 
+    recording_url = request.values.get("RecordingUrl", None)
+ 
+    resp = twilio.twiml.Response()
     resp.say("Thank you. Goodbye.")
-    return str(resp) 
+    
+    return str(resp)
   
 if __name__ == "__main__":
     app.run(debug=True)
